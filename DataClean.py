d85bcd0cc18d38efe27b39d8717ee122dc4c1cf1
marrakechy <belmarrakechy21@coe.edu>
2023-10-18 00:25:15 -0500

    read the files and extract the data

--- DataClean.py
+++ DataClean.py
@@ -1,5 +1,6 @@
 import os
 import re
+from PyPDF2 import PdfReader
 
 def getText(name):
 	list = os.listdir("data\Archive\\"+name)
@@ -35,4 +36,36 @@ def makeTrain(fname, size):
 
 #makeTrain("data/hamText.txt",.85)
 #makeTrain("data/spamText.txt",.85)
+
+
+
+#absolute paths to your directories
+DSResumes_path = r"C:\Users\test1\PycharmProjects\project1-resumefilter-marrakechy\data\DSResume"
+OtherResumes_path = r"C:\Users\test1\PycharmProjects\project1-resumefilter-marrakechy\data\OtherResumes"
+UnknownResumes_path = r"C:\Users\test1\PycharmProjects\project1-resumefilter-marrakechy\data\UnknownResumes"
+
+
+def extract_text_from_pdfs(path, output_file_name):
+	# Get list of PDFs in the specified directory
+	pdf_list = [file for file in os.listdir(path) if file.lower().endswith('.pdf')]
+
+	# Create or overwrite the output text file
+	with open(output_file_name, 'w', encoding='utf-8') as outfile:
+		for pdf in pdf_list:
+			pdf_path = os.path.join(path, pdf)
+
+			# Extract text from the current PDF
+			with open(pdf_path, 'rb') as pdf_file:
+				reader = PdfReader(pdf_file)
+				for page in reader.pages:
+					extracted_text = page.extract_text()
+					if extracted_text:  # Ensure there's text before writing
+						outfile.write(extracted_text)
+
+
+# Extracting texts and writing to respective files
+extract_text_from_pdfs(DSResumes_path, "DSResumes.txt")
+extract_text_from_pdfs(OtherResumes_path, "OtherResumes.txt")
+extract_text_from_pdfs(UnknownResumes_path, "UnknownResumes.txt")
+
 
